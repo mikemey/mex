14b461327dbdbdc054798d48d7ab7b361e32e8d5
mikemey <miguel.maestro@gmx.co.uk>
2019-11-22 14:21:04 +0000

    move + enable register-page test

--- useraccount-test/register.spec.cy.js
+++ useraccount-test/register.spec.cy.js
@@ -6,7 +6,7 @@ describe('Registration', () => {
   describe('page', () => {
     it('has correct form fields', () => {
       cy.get('input#email').should('have.attr', 'type', 'email')
-      cy.get('input#email').should('have.attr', 'placeholder', 'XXX_Email')
+      cy.get('input#email').should('have.attr', 'placeholder', 'Email')
       cy.get('input#password').should('have.attr', 'type', 'password')
       cy.get('input#password-confirm').should('have.attr', 'type', 'password')
     })
