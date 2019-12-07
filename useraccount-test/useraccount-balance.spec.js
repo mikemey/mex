1dbb1b5c74b9149599e6bb1e95047016d3eac343
mikemey <miguel.maestro@gmx.co.uk>
2019-12-07 20:30:16 +0000

    add deposit/withdraw buttons in balance sheet

--- useraccount-test/useraccount-balance.spec.js
+++ useraccount-test/useraccount-balance.spec.js
@@ -14,23 +14,40 @@ describe('UserAccount balance', () => {
   })
   after(() => orchestrator.stop())
 
-  it('user without balance-record', async () => {
-    const res = await useragent.get('/balance')
-    const html = orchestrator.withHtml(res).html
-    html.pageTitle().should.equal('mex balances')
-    html.$('[data-balance="btc"]').text().should.equal('0.00000000')
-    html.$('[data-balance="eth"]').text().should.equal('0.00000000')
+  describe('page', () => {
+    it('user without balance-record', async () => {
+      const res = await useragent.get('/balance')
+      const html = orchestrator.withHtml(res).html
+      html.pageTitle().should.equal('mex balances')
+      html.$('[data-balance="btc"]').text().should.equal('0.00000000')
+      html.$('[data-balance="eth"]').text().should.equal('0.00000000')
+
+      const assertButtons = symbol => {
+        html.$(`[data-deposit="${symbol}"]`).text().should.equal('Deposit')
+        html.$(`[data-withdraw="${symbol}"]`).text().should.equal('Withdraw')
+      }
+      assertButtons('btc')
+      assertButtons('eth')
+    })
+
+    it('existing user with balance', async () => {
+      await balanceColl().insertOne({
+        _id: ObjectId(orchestrator.testUserId),
+        assets: [{ symbol: 'btc', amount: Long.fromString('9223372036854775807') }]
+      })
+      const res = await useragent.get('/balance')
+      const html = orchestrator.withHtml(res).html
+      html.pageTitle().should.equal('mex balances')
+      html.$('[data-balance="btc"]').text().should.equal('92233720368.54775807')
+      html.$('[data-balance="eth"]').text().should.equal('0.00000000')
+    })
   })
 
-  it('existing user with balance', async () => {
-    await balanceColl().insertOne({
-      _id: ObjectId(orchestrator.testUserId),
-      assets: [{ symbol: 'btc', amount: Long.fromString('9223372036854775807') }]
+  describe('deposit', async () => {
+    it('request address from wallet service', async () => {
+      // const res = await useragent.get('/balance/address/btc')
+      // expect call to wallet-service
+      // returns wallet-service response address
     })
-    const res = await useragent.get('/balance')
-    const html = orchestrator.withHtml(res).html
-    html.pageTitle().should.equal('mex balances')
-    html.$('[data-balance="btc"]').text().should.equal('92233720368.54775807')
-    html.$('[data-balance="eth"]').text().should.equal('0.00000000')
   })
 })
