c9ff6282bc0c8d3a787db299fb85e249f0f769b1
mikemey <miguel.maestro@gmx.co.uk>
2019-12-12 13:22:48 +0000

    add request chain check

---
+++ wallet-test/chains/chain.spec.js
@@ -0,0 +1,18 @@
+const chains = require('../../wallet/chains')
+
+describe('Chain module', () => {
+  const supportedAssets = ['btc']
+
+  supportedAssets.forEach(asset => {
+    it('returns known chains', () => {
+      const chain = chains.getChain(asset)
+      chain.symbol.should.equal(asset)
+      chain.should.have.property('create')
+    })
+  })
+
+  it('throws error for unknown assets', () => {
+    const unknownAsset = 'unknown';
+    (() => chains.getChain(unknownAsset)).should.throw(Error, 'chain not supported: ' + unknownAsset)
+  })
+})
