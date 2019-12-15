dea7da8d32ca099228621192a4e603b33529e3a1
mikemey <miguel.maestro@gmx.co.uk>
2019-12-15 23:17:51 +0000

    add fromBtcValue function

--- utils-test/satoshi.spec.js
+++ utils-test/satoshi.spec.js
@@ -3,97 +3,129 @@ const { units: { Satoshi }, dbconnection } = require('../utils')
 const { TestDataSetup: { dropTestDatabase, dbConfig } } = require('../test-tools')
 
 describe('Satoshi', () => {
-  const testcollection = dbconnection.collection('satoshitest')
+  describe('fromString', () => {
+    it('valid create', () => {
+      const strValue = '999999999999999999'
+      Satoshi.fromString(strValue).toString().should.equal(strValue)
+    })
 
-  before(async () => {
-    await dbconnection.connect(dbConfig)
-    await dropTestDatabase()
-  })
-  after(dbconnection.close)
+    it('number > 18 digits not allowed', () => {
+      (() => Satoshi.fromString('1000000000000000000')).should.throw(Error, 'value exceeds 18 digits: 1000000000000000000')
+    })
 
-  it('create from string', () => {
-    const strValue = '999999999999999999'
-    const sat = Satoshi.fromString(strValue)
-    sat.toString().should.equal(strValue)
-  })
+    it('only allow whole number', () => {
+      (() => Satoshi.fromString('1.1')).should.throw(Error, 'only digits allowed: 1.1')
+    })
 
-  it('create from number', () => {
-    const value = Satoshi.fromNumber(1234)
-    value.toString().should.equal('1234')
-  })
+    it('only allows digits', () => {
+      (() => Satoshi.fromString('1,1')).should.throw(Error, 'only digits allowed: 1,1')
+    })
 
-  it('create with constructor', () => {
-    const sat = new Satoshi(-1486618625, 232830643)
-    sat.toString().should.equal('999999999999999999')
-  })
-
-  it('return as BTC string', () => {
-    Satoshi.fromString('123456789012345678')
-      .toBtc().should.equal('1234567890.12345678')
-
-    Satoshi.fromString('1').toBtc().should.equal('0.00000001')
-    Satoshi.fromString('0').toBtc().should.equal('0.00000000')
-    Satoshi.fromString('100000000').toBtc().should.equal('1.00000000')
+    it('negative numbers not allowed', () => {
+      (() => Satoshi.fromString('-1')).should.throw(Error, 'negative value not allowed: -1')
+    })
   })
 
-  it('return as BigInt', () => {
-    (Satoshi.fromString('999999999999999999').toBigInt() === 999999999999999999n)
-      .should.equal(true);
+  describe('create with constructor', () => {
+    it('valid create', () => {
+      const sat = new Satoshi(-1486618625, 232830643)
+      sat.toString().should.equal('999999999999999999')
+    })
 
-    (Satoshi.fromString('1').toBigInt() === 1n).should.equal(true);
-    (Satoshi.fromString('0').toBigInt() === 0n).should.equal(true);
-    (Satoshi.fromString('100000000').toBigInt() === 100000000n).should.equal(true)
+    it('number > 18 digits not allowed', () => {
+      (() => new Satoshi(-1486618624, 232830643)).should.throw(Error, 'value exceeds 18 digits: 1000000000000000000')
+    })
+
+    it('requires 2 args', () => {
+      (() => new Satoshi('9.9')).should.throw(Error, 'Statoshi constructor requires 2 arguments (lowBits, highBits)');
+      (() => new Satoshi()).should.throw(Error, 'Statoshi constructor requires 2 arguments (lowBits, highBits)')
+    })
+
+    it('negative numbers not allowed', () => {
+      (() => new Satoshi(-1, -1)).should.throw(Error, 'negative value not allowed: -1')
+    })
   })
 
-  it('constructor number > 18 digits not allowed', () => {
-    (() => new Satoshi(-1486618624, 232830643)).should.throw(Error, 'value exceeds 18 digits: 1000000000000000000')
+  describe('fromBtcValue', () => {
+    it('valid create', () => {
+      Satoshi.fromBtcValue(1.234).toString().should.equal('123400000')
+      Satoshi.fromBtcValue(1).toString().should.equal('100000000')
+      Satoshi.fromBtcValue(1.0).toString().should.equal('100000000')
+      Satoshi.fromBtcValue(1.1).toString().should.equal('110000000')
+      Satoshi.fromBtcValue(0.001).toString().should.equal('100000')
+      Satoshi.fromBtcValue(0.0000054).toString().should.equal('540')
+      Satoshi.fromBtcValue(999999.99999999).toString().should.equal('99999999999999')
+      Satoshi.fromBtcValue(999999).toString().should.equal('99999900000000')
+    })
+
+    it('precision > 8 not allowed', () => {
+      (() => Satoshi.fromBtcValue(1.123456789)).should.throw(Error, 'invalid precision: 1.123456789')
+    })
+
+    it('btc >= 1 million not allowed', () => {
+      (() => Satoshi.fromBtcValue(1000000)).should.throw(Error, 'too large: 1000000')
+    })
+
+    it('negative btc not allowed', () => {
+      (() => Satoshi.fromBtcValue(-1)).should.throw(Error, 'negative value not allowed: -1')
+    })
   })
 
-  it('fromString number > 18 digits not allowed', () => {
-    (() => Satoshi.fromString('1000000000000000000')).should.throw(Error, 'value exceeds 18 digits: 1000000000000000000')
+  describe('convert value', () => {
+    it('to BTC string', () => {
+      Satoshi.fromString('123456789012345678')
+        .toBtc().should.equal('1234567890.12345678')
+
+      Satoshi.fromString('1').toBtc().should.equal('0.00000001')
+      Satoshi.fromString('0').toBtc().should.equal('0')
+      Satoshi.fromString('100000000').toBtc().should.equal('1')
+    })
+
+    it('to/from BTC string', () => {
+      Satoshi.fromBtcValue(1.234).toBtc().should.equal('1.234')
+      Satoshi.fromBtcValue(1).toBtc().should.equal('1')
+      Satoshi.fromBtcValue(1.0).toBtc().should.equal('1')
+      Satoshi.fromBtcValue(1.1).toBtc().should.equal('1.1')
+      Satoshi.fromBtcValue(0.001).toBtc().should.equal('0.001')
+      Satoshi.fromBtcValue(0.0000054).toBtc().should.equal('0.0000054')
+      Satoshi.fromBtcValue(999999.99999999).toBtc().should.equal('999999.99999999')
+      Satoshi.fromBtcValue(999999).toBtc().should.equal('999999')
+    })
+
+    it('to BigInt', () => {
+      (Satoshi.fromString('999999999999999999').toBigInt() === 999999999999999999n)
+        .should.equal(true);
+
+      (Satoshi.fromString('1').toBigInt() === 1n).should.equal(true);
+      (Satoshi.fromString('0').toBigInt() === 0n).should.equal(true);
+      (Satoshi.fromString('100000000').toBigInt() === 100000000n).should.equal(true)
+    })
   })
 
-  it('fromNumber number > 15 digits not allowed', () => {
-    (() => Satoshi.fromNumber(1234567890123456)).should.throw(Error, 'value exceeds 15 digits (when converting from number): 1234567890123456')
+  describe('illegal create functions', () => {
+    it('fromInt not allowed', () => {
+      (() => Satoshi.fromInt(1234)).should.throw(Error, 'not supported, use "fromString"')
+    })
+
+    it('fromNumber not allowed', () => {
+      (() => Satoshi.fromNumber(1234)).should.throw(Error, 'not supported, use "fromString"')
+    })
   })
 
-  it('constructor requires 2 args', () => {
-    (() => new Satoshi('9.9')).should.throw(Error, 'Statoshi constructor requires 2 arguments (lowBits, highBits)');
-    (() => new Satoshi()).should.throw(Error, 'Statoshi constructor requires 2 arguments (lowBits, highBits)')
-  })
+  describe('store in db', () => {
+    const testcollection = dbconnection.collection('satoshitest')
 
-  it('fromString only allow whole number', () => {
-    (() => Satoshi.fromString('1.1')).should.throw(Error, 'only digits allowed: 1.1')
-  })
+    before(async () => {
+      await dbconnection.connect(dbConfig)
+      await dropTestDatabase()
+    })
+    after(dbconnection.close)
 
-  it('fromString only allows digits', () => {
-    (() => Satoshi.fromString('1,1')).should.throw(Error, 'only digits allowed: 1,1')
-  })
-
-  it('fromNumber only allow whole number', () => {
-    (() => Satoshi.fromNumber(1.1)).should.throw(Error, 'only whole numbers: 1.1')
-  })
-
-  it('constructor negative numbers not allowed', () => {
-    (() => new Satoshi(-1, -1)).should.throw(Error, 'negative value not allowed: -1')
-  })
-
-  it('fromString negative numbers not allowed', () => {
-    (() => Satoshi.fromString('-1')).should.throw(Error, 'negative value not allowed: -1')
-  })
-
-  it('fromNumber negative numbers not allowed', () => {
-    (() => Satoshi.fromNumber(-1)).should.throw(Error, 'negative value not allowed: -1')
-  })
-
-  it('fromInt not allowed', () => {
-    (() => Satoshi.fromInt(1234)).should.throw(Error, 'not supported, use "fromString"')
-  })
-
-  it('can store and retrieve from DB', async () => {
-    const strValue = '999999999999999999'
-    const saved = await testcollection.insertOne({ testval: Satoshi.fromString(strValue) })
-    const doc = await testcollection.findOne({ _id: saved.insertedId })
-    doc.testval.toString().should.equal(strValue)
+    it('can store and retrieve', async () => {
+      const strValue = '999999999999999999'
+      const saved = await testcollection.insertOne({ testval: Satoshi.fromString(strValue) })
+      const doc = await testcollection.findOne({ _id: saved.insertedId })
+      doc.testval.toString().should.equal(strValue)
+    })
   })
 })
--- utils/units.js
+++ utils/units.js
@@ -1,8 +1,8 @@
 const Long = require('mongodb').Long
 
 const BTC_FRACTION_COUNT = 8
+const BTC_MAX_VALUE = 1000000
 const FROM_STRING_MAX_LEN = 18
-const FROM_NUMBER_MAX_LEN = 999999999999999
 const SATOSHI_MIN = Long.fromInt(0)
 
 const checkMax = strValue => {
@@ -31,15 +31,7 @@ class Satoshi extends Long {
 
   static fromInt () { throw new Error('not supported, use "fromString"') }
 
-  static fromNumber (num) {
-    if (num < 0) { return negativeValueError(num) }
-    if (num % 1 !== 0) { throw new Error(`only whole numbers: ${num}`) }
-    if (num > FROM_NUMBER_MAX_LEN) {
-      throw new Error(`value exceeds 15 digits (when converting from number): ${num}`)
-    }
-    // return this.fromString(`000${num}`)
-    return super.fromNumber(num)
-  }
+  static fromNumber (num) { throw new Error('not supported, use "fromString"') }
 
   static fromString (value) {
     checkMax(value)
@@ -49,15 +41,27 @@ class Satoshi extends Long {
     return new Satoshi(lval.low_, lval.high_)
   }
 
-  toString () { return super.toString() }
+  static fromBtcValue (btcValue) {
+    if (btcValue >= BTC_MAX_VALUE) { throw new Error(`too large: ${btcValue}`) }
+    let btcStr = String(btcValue)
+    const dotIndex = btcStr.indexOf('.')
+    if ((btcStr.length - dotIndex - 1) > BTC_FRACTION_COUNT) { throw new Error(`invalid precision: ${btcValue}`) }
+    btcStr = btcStr.replace('.', '')
+    const satoshis = (dotIndex > 0)
+      ? btcStr.padEnd(dotIndex + BTC_FRACTION_COUNT, '0')
+      : `${btcStr}00000000`
+    return this.fromString(satoshis)
+  }
 
   toBigInt () { return BigInt(this.toString()) }
 
   toBtc () {
     const value = this.toString().padStart(BTC_FRACTION_COUNT + 1, '0')
     const whole = value.slice(0, -BTC_FRACTION_COUNT)
-    const fraction = value.slice(-BTC_FRACTION_COUNT)
-    return `${whole}.${fraction}`
+    const fraction = value.slice(-BTC_FRACTION_COUNT).replace(/[0]*$/, '')
+    return fraction.length > 0
+      ? `${whole}.${fraction}`
+      : whole
   }
 }
 
