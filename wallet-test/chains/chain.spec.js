25141ca4df0cb4cf449b462b85fe018c22067179
mikemey <miguel.maestro@gmx.co.uk>
2019-12-15 07:15:49 +0000

    implement create-/stop-all chains

--- wallet-test/chains/chain.spec.js
+++ wallet-test/chains/chain.spec.js
@@ -1,18 +1,52 @@
 const chains = require('../../wallet/chains')
+const btcnodeOrch = require('../chains/btc-node.orch')
 
 describe('Chain module', () => {
-  const supportedAssets = ['btc']
+  const chainsConfig = { btcnode: btcnodeOrch.defaultBtcAdapterConfig }
 
-  supportedAssets.forEach(asset => {
-    it('returns known chains', () => {
-      const chain = chains.getChain(asset)
-      chain.symbol.should.equal(asset)
-      chain.should.have.property('start')
+  describe('getChainAdapter', () => {
+    beforeEach(() => chains.createAll(chainsConfig))
+    afterEach(() => chains.stopAll())
+
+    const supportedAssets = ['btc']
+
+    supportedAssets.forEach(asset => {
+      it('returns known chains', () => {
+        const chain = chains.getChainAdapter(asset)
+
+        chain.should.have.property('startListener')
+        chain.should.have.property('stopListener')
+        chain.should.have.property('createNewAddress')
+      })
     })
   })
 
-  it('throws error for unknown assets', () => {
-    const unknownAsset = 'unknown';
-    (() => chains.getChain(unknownAsset)).should.throw(Error, 'chain not supported: ' + unknownAsset)
+  describe('usage error', () => {
+    it('throws error when adapters not created', () => {
+      (() => chains.getChainAdapter('whateva')).should.throw(Error, 'chain adapters not created')
+    })
+
+    it('throws error for unknown assets', () => {
+      chains.createAll(chainsConfig)
+      const unknownAsset = 'unknown';
+      (() => chains.getChainAdapter(unknownAsset)).should.throw(Error, 'chain not supported: ' + unknownAsset)
+    })
+  })
+
+  describe('configuration errors', () => {
+    const testParameters = [
+      { title: 'missing btcnode configuration', changeConfig: cfg => delete cfg.btcnode, error: '"btcnode" is required' }
+    ]
+
+    testParameters.forEach(params => {
+      it(params.title, () => {
+        const config = {
+          btcnode: { does: 'not-matter' }
+        }
+
+        params.changeConfig(config);
+        (() => chains.createAll(config)).should.throw(Error, params.error)
+      })
+    })
   })
 })
--- wallet/chains/index.js
+++ wallet/chains/index.js
@@ -1,15 +1,39 @@
+const Joi = require('@hapi/joi')
+
 const chains = [
   require('./btc-adapter')
 ]
+const { Validator } = require('../../utils')
 
-const chainNotSupported = asset => { throw new Error(`chain not supported: ${asset}`) }
+const configSchema = Joi.object({
+  btcnode: Joi.object().required()
+})
 
-const getAdapter = asset =>
-  chains.find(chain => chain.symbol === asset) ||
-  chainNotSupported(asset)
+const data = {
+  availableSymbols: chains.map(chain => chain.symbol),
+  chainAdapters: null
+}
 
-const getChainAdapter = () => { }
-const createAll = allConfigs => { }
-const stopAll = () => { }
+const createAll = allConfigs => {
+  Validator.oneTimeValidation(configSchema, allConfigs)
+
+  const getConfigFor = chain => allConfigs[chain.symbol + 'node']
+  data.chainAdapters = new Map(
+    chains.map(chain => [chain.symbol, chain.create(getConfigFor(chain))])
+  )
+}
+
+const getChainAdapter = symbol => {
+  if (!data.chainAdapters) { throw new Error('chain adapters not created') }
+  if (!data.availableSymbols.includes(symbol)) { throw new Error(`chain not supported: ${symbol}x`) }
+  return data.chainAdapters.get(symbol)
+}
+
+const stopAll = () => {
+  for (const adapter of data.chainAdapters.values()) {
+    adapter.stopListener()
+  }
+  data.chainAdapters = null
+}
 
 module.exports = { createAll, stopAll, getChainAdapter }
