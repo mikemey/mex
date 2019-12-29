08256c0c619b2532b92ba9cb028e34cfd30a3427
mikemey <miguel.maestro@gmx.co.uk>
2019-12-29 12:03:10 +0100

    wip #11: implement soclient sunny path

--- connectors-test/interceptor/soserver-interceptor.js
+++ connectors-test/interceptor/soserver-interceptor.js
@@ -1,109 +1,74 @@
-const uws = require('uWebSockets.js')
+const { Router } = require('zeromq')
 const {
   Logger,
-  wsmessages: { createRawMessage, extractMessage, OK_STATUS }
+  wsmessages: { parseMessageBody, createMessageBody, OK_STATUS }
 } = require('../../utils')
 
-const closeClientSockets = clients => clients.forEach(client => client.close())
-
-class WSServerInterceptor {
-  constructor (port, path) {
-    this.port = port
-    this.path = path
-    this.listenSocket = null
-    this.defaultResponse = { status: OK_STATUS }
-    this._defaultInterceptors = {
-      responsePromise: () => Promise.resolve(this.defaultResponse),
-      stopProcessing: false,
-      afterResponse: null
+const AuthHandler = receivedAuthTokens => {
+  const socket = new Router()
+  const run = async () => {
+    await socket.bind('inproc://zeromq.zap.01')
+    for await (const request of socket) {
+      const [path, delimiter, version, id] = request
+      const [mechanism, user, authToken] = request.slice(7).map(p => p.toString())
+      receivedAuthTokens.push({ mechanism, user, authToken })
+      const response = [path, delimiter, version, id, '200', 'OK', null, null]
+      await socket.send(response)
     }
-    this.interceptors = {}
-    this.resetInterceptors()
-    this.logger = Logger(this.constructor.name)
   }
 
-  resetInterceptors () {
-    this.interceptors = Object.assign({}, this._defaultInterceptors)
-  }
-
-  start () {
-    this.clients = []
-    this.received = { authTokens: [], messages: [] }
-    return new Promise((resolve, reject) => {
-      if (this.listenSocket) {
-        return reject(Error('WSServerInterceptor already started'))
-      }
-      uws.App({}).ws(this.path, {
-        open: (ws, req) => {
-          this.logger.debug('incoming connection')
-          this.received.authTokens.push(req.getHeader('x-auth-token'))
-          this.clients.push(ws)
-        },
-        message: (ws, buffer) => this._processMessage(ws, buffer),
-        close: closingWs => {
-          this.logger.debug('socket closed')
-          this._removeClient(closingWs)
-        }
-      }).listen(this.port, socket => {
-        if (socket) {
-          this.logger.debug(`listening on: ${this.port}`)
-          this.listenSocket = socket
-          resolve()
-        }
-      })
-    })
-  }
-
-  _removeClient (client) {
-    this.clients = this.clients.filter(c => c !== client)
-  }
-
-  _processMessage (ws, buffer) {
-    let receivedMessageId = ''
-    return new Promise((resolve, reject) => {
-      try {
-        const rawMessage = String.fromCharCode.apply(null, new Uint8Array(buffer))
-        const message = extractMessage(rawMessage)
-        receivedMessageId = message.id
-        this.logger.debug(`received: <${message.id}>`, message.body)
-        resolve(message.body)
-      } catch (err) { reject(err) }
-    }).then(request => {
-      this.received.messages.push(request)
-      return this.interceptors.responsePromise(ws)
-    }).then(response => {
-      if (this.interceptors.stopProcessing) {
-        this.logger.debug('interceptors.stopProcessing flag is True')
-        return
-      }
-      this.logger.debug(`responding: <${receivedMessageId}>`, response)
-      return ws.send(createRawMessage(receivedMessageId, response))
-    }).then(sendResultOk => {
-      if (this.interceptors.stopProcessing) { return }
-      const buffered = ws.getBufferedAmount()
-      this.logger.debug(`send result: ${sendResultOk}, backpressure: ${buffered}`)
-      if (!sendResultOk || buffered > 0) { throw new Error('WSServerInterceptor: sending failed') }
-      if (this.interceptors.afterResponse) {
-        return this.interceptors.afterResponse(ws)
-      }
-    }).catch(err => {
-      this.logger.debug('processing error:', err)
-      this._removeClient(ws)
-    })
-  }
-
-  stop () {
-    return new Promise(resolve => {
-      this.logger.debug('stop')
-      if (this.listenSocket) {
-        this.logger.debug('closing all sockets...')
-        closeClientSockets(this.clients)
-        uws.us_listen_socket_close(this.listenSocket)
-        this.listenSocket = null
-      }
-      resolve()
-    })
-  }
+  run()
+  return { close: () => socket.close() }
 }
 
-module.exports = WSServerInterceptor
+const ServerInterceptor = address => {
+  const sockets = {
+    authHandler: null,
+    server: null
+  }
+
+  const logger = Logger('server-mock-interceptor')
+  const received = { authTokens: [], messages: [] }
+  const defaultResponse = { status: OK_STATUS }
+  const defaultInterceptors = { responsePromise: () => Promise.resolve(defaultResponse) }
+  const interceptors = { responsePromise: null }
+
+  const resetInterceptors = () => {
+    interceptors.responsePromise = defaultInterceptors.responsePromise
+  }
+
+  const start = async () => {
+    resetInterceptors()
+    received.authTokens = []
+    received.messages = []
+
+    sockets.authHandler = AuthHandler(received.authTokens)
+    sockets.server = new Router()
+    sockets.server.plainServer = true
+    await sockets.server.bind(address)
+    serverLoop()
+  }
+
+  const serverLoop = async () => {
+    for await (const [path, mid, mbody] of sockets.server) {
+      const data = parseMessageBody(mbody)
+      logger.debug('received:', data)
+      received.messages.push(data)
+      const responseData = await interceptors.responsePromise(sockets.server)
+      logger.debug('response:', responseData)
+
+      await sockets.server.send([path, mid, createMessageBody(responseData)])
+    }
+  }
+
+  const stop = () => {
+    if (sockets.authHandler) { sockets.authHandler.close() }
+    if (sockets.server) { sockets.server.close() }
+    sockets.authHandler = null
+    sockets.server = null
+  }
+
+  return { start, stop, interceptors, resetInterceptors, defaultResponse, received }
+}
+
+module.exports = ServerInterceptor
--- connectors/soclient.js
+++ connectors/soclient.js
@@ -1,236 +1,61 @@
-const WebSocket = require('ws')
+const { Dealer } = require('zeromq')
 const Joi = require('@hapi/joi')
 
-const { Logger, Validator, wsmessages } = require('../utils')
+const { Logger, Validator, wsmessages: { createMessage, parseMessage } } = require('../utils')
+const connectionUser = 'user'
 
 const configSchema = Joi.object({
-  url: Joi.string().uri().required(),
+  address: Joi.string().uri().required(),
   authToken: Validator.secretToken('authToken'),
-  timeout: Joi.number().min(20).max(60000).required(),
-  pingInterval: Joi.number().min(20).max(100000).required()
+  timeout: Joi.number().min(20).max(60000).required()
 })
 
-const WS_WAIT_RETRIES = 5
+const SocketClient = (config, logCategory) => {
+  Validator.oneTimeValidation(configSchema, config)
+  if (!logCategory) { throw Error('logCategory required') }
 
-const Waiter = (ws, reject) => {
-  let retries = WS_WAIT_RETRIES
+  const logger = Logger(logCategory)
+  const client = new Dealer()
+  const messageHandlers = new Map()
 
-  const checkStatus = (desiredState, resolve = err => { throw err }) => {
-    if (ws.readyState === desiredState) {
-      return resolve()
+  const start = async () => {
+    client.plainUsername = connectionUser
+    client.plainPassword = config.authToken
+
+    // client.events.on('handshake:error:auth', data => {
+    //   logger.error('auth error:', data.address, data.error)
+    // })
+    logger.info('connecting to:', config.address)
+    await client.connect(config.address)
+    listenLoop()
+  }
+
+  const listenLoop = async () => {
+    for await (const rawmessage of client) {
+      const [mid, data] = parseMessage(rawmessage)
+      logger.debug('received:', mid, data)
+      messageHandlers.get(mid).resolve(data)
+      messageHandlers.delete(mid)
     }
-    retries--
-    if (retries > 0) {
-      setTimeout(() => checkStatus(desiredState, resolve), 10)
-    } else {
-      reject(Error(`waiting for socket state ${desiredState} exceeded retries (max: ${WS_WAIT_RETRIES})`))
-    }
-  }
-  return checkStatus
-}
-
-class TimeoutError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = this.constructor.name
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
-
-const topicSubscription = topic => wsmessages.withAction('subscribe').build({ topic })
-const topicUnsubscribe = topic => wsmessages.withAction('unsubscribe').build({ topic })
-
-class WSClient {
-  constructor (config, logCategory) {
-    if (!logCategory) { throw Error('logCategory required') }
-    Validator.oneTimeValidation(configSchema, config)
-
-    this.logger = Logger(logCategory)
-    this.wsconfig = config
-    this._reset()
   }
 
-  _reset (callback = () => { }) {
-    this.logger.debug('resetting state')
-    clearInterval(this.wsping)
-    this._clearListeners(true)
-    this.ws = null
-    this.headers = { 'X-AUTH-TOKEN': this.wsconfig.authToken }
-    this.messageHandler = {}
-    this.topicHandler = {}
-    callback()
-  }
-
-  _openWebsocket (resolve, reject) {
-    this.logger.info('connecting to:', this.wsconfig.url)
-
-    const saveEnding = (endCb, ...args) => {
-      this._clearListeners()
-      this.logger.debug(...args)
-      endCb()
-    }
-
-    const connectTimeout = this._createTimeout(
-      err => saveEnding(() => reject(err), 'connecting timed out'),
-      'connecting timed out'
-    )
-
-    this.ws = new WebSocket(this.wsconfig.url, { headers: this.headers })
-    this.ws.prependOnceListener('open', () => {
-      connectTimeout.cancel()
-      clearInterval(this.wsping)
-      this.wsping = setInterval(() => this.ws.ping(), this.wsconfig.pingInterval)
-      saveEnding(resolve, 'connection established')
-    })
-
-    this.ws.addListener('message', raw => {
-      const message = wsmessages.extractMessage(raw)
-      const handler = message.isBroadcast ? this.topicHandler[message.topic] : this.messageHandler[message.id]
-      const errorId = message.isBroadcast ? message.topic : message.id
-      return handler ? handler(message) : this.logger.info('dropping received:', `<${errorId}>`)
-    })
-
-    const closedown = error => {
-      connectTimeout.cancel()
-      saveEnding(() => reject(error), 'connection error:', error)
-    }
-    this.ws.prependOnceListener('close', closedown)
-    this.ws.prependOnceListener('error', closedown)
-    this.ws.prependOnceListener('unexpected-response', () => closedown(Error('unexpected-response')))
-  }
-
-  send (request) {
-    return this._internalSend(request)
-  }
-
-  _internalSend (request, callback) {
+  const send = data => {
+    const message = createMessage(data)
+    const [mid, rawdata] = message
     return new Promise((resolve, reject) => {
-      const waitfor = Waiter(this.ws, reject)
-      const interceptResponse = callback ? callback(resolve) : resolve
-      const sendMessage = () => this._requestResponse(request, interceptResponse, reject)
-      const openAndSendMessage = () => this._openWebsocket(sendMessage, reject)
-
-      const state = this.ws === null ? WebSocket.CLOSED : this.ws.readyState
-      switch (state) {
-        case WebSocket.CLOSED: {
-          this.logger.debug('socket closed, openAndSendMessage')
-          return openAndSendMessage()
-        }
-        case WebSocket.CLOSING: {
-          this.logger.debug('socket closing, waitfor(WebSocket.CLOSED, openAndSendMessage)')
-          return waitfor(WebSocket.CLOSED, openAndSendMessage)
-        }
-        case WebSocket.CONNECTING: {
-          this.logger.debug('socket connecting, waitfor(WebSocket.OPEN, sendMessage)')
-          return waitfor(WebSocket.OPEN, sendMessage)
-        }
-        case WebSocket.OPEN: return sendMessage()
-        default: return reject(Error(`unexpected WebSocket state [${this.ws.readyState}]`))
-      }
+      logger.debug('sending:', mid, rawdata)
+      messageHandlers.set(mid, { resolve, reject })
+      return client.send(message)
     }).catch(err => {
-      this.logger.error('processing error:', err.message, err)
-      const sendError = err instanceof TimeoutError ? err : new Error('disconnected')
-      this._stopSync()
-      throw sendError
+      logger.error('sending error:', mid, err.message, err)
+      messageHandlers.get(mid).reject(new Error('disconnected'))
+      messageHandlers.delete(mid)
     })
   }
 
-  subscribe (topic, callback = (topic, message) => { }) {
-    const addTopicCallback = resolve => subscriptionResponse => {
-      if (subscriptionResponse.status === wsmessages.OK_STATUS) {
-        this.topicHandler[topic] = message => {
-          this.logger.debug('received:', `<${topic}>`, message.body)
-          callback(topic, message.body)
-        }
-      }
-      resolve(subscriptionResponse)
-    }
-    return this._internalSend(topicSubscription(topic), addTopicCallback)
-  }
+  const stop = () => client.close()
 
-  unsubscribe (topic) {
-    return this._internalSend(topicUnsubscribe(topic))
-  }
-
-  _requestResponse (request, resolve, reject) {
-    const sendingId = wsmessages.randomMessageId()
-
-    const saveEnding = (func, obj, logFunc, ...logArgs) => {
-      this._clearListeners()
-      delete this.messageHandler[sendingId]
-      logFunc(...logArgs)
-      func(obj)
-    }
-
-    const responseTimeout = this._createTimeout(
-      err => saveEnding(reject, err, this.logger.error, err.message),
-      'response timed out'
-    )
-
-    const handler = message => {
-      responseTimeout.cancel()
-      saveEnding(resolve, message.body, this.logger.debug, 'received:', `<${message.id}>`, message.body)
-    }
-
-    const saveReject = (err, message) => {
-      responseTimeout.cancel()
-      saveEnding(reject, err, this.logger.error, message)
-    }
-
-    this.messageHandler[sendingId] = handler
-    this.ws.prependOnceListener('close', () => saveReject(new TimeoutError('remote socket closed'), 'requestResponse close'))
-    this.ws.prependOnceListener('unexpected-response', err => saveReject(err, 'requestResponse unexpected-response'))
-    this.ws.prependOnceListener('error', err => saveReject(err, 'requestResponse error'))
-
-    this.logger.debug('sending:', `<${sendingId}>`, request)
-    this.ws.send(wsmessages.createRawMessage(sendingId, request), err => err
-      ? saveReject(err, 'sending error')
-      : this.logger.debug('sending done')
-    )
-  }
-
-  stop () {
-    return new Promise((resolve) => this._stopSync(resolve))
-  }
-
-  _stopSync (resolve = () => { }) {
-    this.logger.debug('closing connection...')
-    if (this.ws === null) return resolve()
-    try {
-      this.ws.close()
-      this.logger.info('stopped')
-    } catch (err) {
-      this.logger.error('error stopping:', err.message)
-    }
-    this._reset(resolve)
-  }
-
-  _createTimeout (reject, message) {
-    this.logger.debug('creating timeout:', message)
-    const timeout = setTimeout(() => {
-      this.logger.debug('timed out:', message)
-      reject(new TimeoutError(message))
-    }, this.wsconfig.timeout)
-
-    const cancel = () => {
-      this.logger.debug('cancel timeout:', message)
-      clearTimeout(timeout)
-    }
-    return { cancel }
-  }
-
-  _clearListeners (all = false) {
-    if (this.ws != null) {
-      this.ws.removeAllListeners('open')
-      this.ws.removeAllListeners('close')
-      this.ws.removeAllListeners('error')
-      this.ws.removeAllListeners('unexpected-response')
-      this.ws.removeAllListeners('close')
-      if (all) {
-        this.ws.removeAllListeners('message')
-      }
-    }
-  }
+  return { start, send, stop }
 }
 
-module.exports = WSClient
+module.exports = SocketClient
